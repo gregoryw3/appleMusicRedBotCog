ab92dbb56bbb12b73143c551d21401e9c4195f88
Gregory Wainer <greg.wainer@gmail.com>
2022-04-16 15:56:11 -0400

    initial 2

--- AppleMusic/AppleMusic.py
+++ AppleMusic/AppleMusic.py
@@ -1,13 +1,26 @@
+from typing import Literal
+
+import discord
 from redbot.core import commands
+from redbot.core.bot import Red
+from redbot.core.config import Config
+
+RequestType = Literal["discord_deleted_user", "owner", "user", "user_strict"]
+
 
 class AppleMusic(commands.Cog):
-    """My custom cog"""
+    """
+    Adds Apple Music Playback
+    """
 
-    def __init__(self, bot):
+    def __init__(self, bot: Red) -> None:
         self.bot = bot
+        self.config = Config.get_conf(
+            self,
+            identifier='gregoryw3#7813',
+            force_registration=True,
+        )
 
-    @commands.command()
-    async def mycom(self, ctx):
-        """This does stuff!"""
-        # Your code will go here
-        await ctx.send("I can do stuff!")
+    async def red_delete_data_for_user(self, *, requester: RequestType, user_id: int) -> None:
+        # TODO: Replace this with the proper end user data removal handling.
+        super().red_delete_data_for_user(requester=requester, user_id=user_id)
--- AppleMusic/_init_.py
+++
@@ -1,5 +0,0 @@
-from .AppleMusic import AppleMusic
-
-
-def setup(bot):
-    bot.add_cog(AppleMusic(bot))
